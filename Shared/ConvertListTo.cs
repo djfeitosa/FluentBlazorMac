0ecdb38aeb71fa50f1f7db654e37299852e50214
djfeitosa <50143735+djfeitosa@users.noreply.github.com>
2024-01-22 16:42:26 -0300

    Geração de Excel OK, agora verei a parte de geração de PDF

--- Shared/ConvertListTo.cs
+++ Shared/ConvertListTo.cs
@@ -21,29 +21,7 @@ namespace FluentBlazorMac.Shared
             return option;
         }
 
-        public void ConvertToExcel(List<Aluno> lista, string[] columnHeader)
-        {
-            using (var workbook = new XLWorkbook())
-            {
-                var worksheet = workbook.Worksheets.Add("Alunos");
-
-                for (int i = 0; i < columnHeader.Length; i++)
-                {
-                    worksheet.Cell(1, i + 1).Value = columnHeader[i];
-                }
-
-                for (int i = 0; i < lista.Count; i++)
-                {
-                    worksheet.Cell(i + 2, 1).Value = lista[i].Nome;
-                    worksheet.Cell(i + 2, 2).Value = lista[i].Email;
-                    worksheet.Cell(i + 2, 3).Value = lista[i].Idade;
-                    worksheet.Cell(i + 2, 4).Value = lista[i].Turma.Descricao;
-                }
-                SavelocalFile(workbook);
-            }
-        }
-
-        public void SavelocalFile(XLWorkbook workbook)
+        public void SavelocalFile(XLWorkbook workbook, string nomeArquivo)
         {
 
             using (var stream = new MemoryStream())
@@ -51,7 +29,7 @@ namespace FluentBlazorMac.Shared
                 workbook.SaveAs(stream);
                 var content = stream.ToArray();
                 var contentType = "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet";
-                var fileName = "planilha.xlsx";
+                var fileName = $"{nomeArquivo}.xlsx";
                 Microsoft.AspNetCore.Mvc.FileContentResult file = new Microsoft.AspNetCore.Mvc.FileContentResult(content, contentType);
                 file.FileDownloadName = fileName;
                 var path = @"/Users/djalmaf/Desenvolvimento/VS22/FluentBlazorMac/ArquivosExcel/" + fileName;
